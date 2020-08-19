be3d8a4533b87f76d4123b383d697336b12b6019
DomitriusAnthony <domitriusaclark@gmail.com>
2020-08-19 00:09:31 -0400

    Added base to graphcms example

---
+++ examples/mdnext-graphcms/pages/blogs/search.js
@@ -0,0 +1,58 @@
+import { Flex } from '@chakra-ui/core';
+
+import glob from 'fast-glob';
+import fs from 'fs';
+import matter from 'gray-matter';
+
+import ContentBox from '@components/ContentBox';
+import Search from '@components/Search';
+import { Chakra } from '@components/Chakra';
+
+export default function SearchPage({ allMdx }) {
+  const [filteredBlogs, setFilteredBlogs] = React.useState(allMdx);
+
+  const handleFilter = (data) => {
+    setFilteredBlogs(data);
+  };
+
+  return (
+    <Chakra evaluateThemeLazily>
+      <Flex>
+        {/* Content Area + Input + Tag filter */}
+        <Flex direction="column" justify="center" alignItems="center" w="100%">
+          <Search blogs={allMdx} handleFilter={handleFilter} />
+          <Flex direction="column" justify="space-evenly" h="80vh">
+            {filteredBlogs &&
+              filteredBlogs.map((blog) => (
+                <ContentBox key={blog.slug} blog={blog} />
+              ))}
+          </Flex>
+        </Flex>
+      </Flex>
+    </Chakra>
+  );
+}
+
+export function getStaticProps() {
+  const files = glob.sync('src/blogs/*.mdx');
+
+  const allMdx = files.map((file) => {
+    const split = file.split('/');
+    const filename = split[split.length - 1];
+    const slug = filename.replace('.mdx', '');
+
+    const mdxSource = fs.readFileSync(file);
+    const { data } = matter(mdxSource);
+
+    return {
+      slug,
+      ...data,
+    };
+  });
+
+  return {
+    props: {
+      allMdx,
+    },
+  };
+}
---
+++ examples/mdnext-graphcms/pages/index.js
@@ -0,0 +1,13 @@
+import { Flex } from '@chakra-ui/core';
+
+import { Chakra } from '@components/Chakra';
+
+export default function Index({ cookies }) {
+  return (
+    <Chakra cookies={cookies}>
+      <Flex direction="column"></Flex>
+    </Chakra>
+  );
+}
+
+export { getServerSideProps } from '@components/Chakra';
