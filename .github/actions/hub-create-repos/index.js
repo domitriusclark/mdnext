aeec4d7fa8cbb3f8fb2dcd63f2701665084f8ab1
DomitriusAnthony <domitriusaclark@gmail.com>
2020-08-28 14:05:02 -0400

    removing logs

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -4,7 +4,6 @@ const fs = require("fs")
 async function run() {
   try {
     const dirs = fs.readdirSync(process.env.GITHUB_WORKSPACE + '/templates')
-    console.log(dirs);
     for (let dir of dirs) {
       exec(`gh repo create ${dir} --public --enable-issues=false`)
     }
