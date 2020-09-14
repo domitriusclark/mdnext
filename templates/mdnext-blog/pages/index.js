e23257df21ff09bb33ed242389de1064e034184b
DomitriusAnthony <domitriusaclark@gmail.com>
2020-09-14 03:01:12 -0400

    Adding new config for component lib

--- templates/mdnext-blog/pages/index.js
+++ templates/mdnext-blog/pages/index.js
@@ -1,11 +1,14 @@
 import { Flex } from '@chakra-ui/core';
 
 import { Layout } from '@components/Layout';
+import { Image } from '@mdnext/components';
 
 export default function Index({ cookies }) {
   return (
     <Layout>
-      <Flex direction="column"></Flex>
+      <Flex direction="column">
+        <Image publicId="mdnext-light" />
+      </Flex>
     </Layout>
   );
 }
