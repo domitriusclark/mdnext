e76983dd25ad1b8052fdbe27c4ad407bf2dd64b6
DomitriusAnthony <domitriusaclark@gmail.com>
2020-09-14 14:39:24 -0400

    Removing Image import from blog

--- templates/mdnext-blog/pages/index.js
+++ templates/mdnext-blog/pages/index.js
@@ -1,14 +1,10 @@
 import { Flex } from '@chakra-ui/core';
-
 import { Layout } from '@components/Layout';
-import { Image } from '@mdnextjs/components';
 
 export default function Index() {
   return (
     <Layout>
-      <Flex direction="column">
-        <Image publicId="mdnext-light" cloudName="mdnextjs" />
-      </Flex>
+      <Flex direction="column"></Flex>
     </Layout>
   );
 }
