b87bd989fc3720f8d3b369b821dd16bd447660ac
DomitriusAnthony <domitriusaclark@gmail.com>
2020-09-14 15:18:46 -0400

    Adding missing React import for Code component

--- packages/mdnextjs-components/src/components/Code.js
+++ packages/mdnextjs-components/src/components/Code.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/vsDark';
 import { useClipboard } from '@chakra-ui/core';
