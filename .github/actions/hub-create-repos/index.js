0167efaa0fd34231ff9f746bf523e59a7ba23db6
DomitriusAnthony <domitriusaclark@gmail.com>
2020-09-14 22:13:27 -0400

    Still trying to fix the action

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -1,12 +1,10 @@
 const { exec } = require('child_process');
 const fs = require('fs');
-const { stdout } = require('process');
 
 async function run() {
   try {
     const dirs = fs.readdirSync(process.env.GITHUB_WORKSPACE + '/templates');
-    exec(`gh auth status`);
-    console.log(stdout);
+    exec(`gh auth login`);
     for (let dir of dirs) {
       exec(`gh repo create ${dir} --public --enable-issues=false`);
     }
