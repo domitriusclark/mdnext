60c51ecbec1789641854716a4fb1ede2aaa06a22
Eka <6597211+ekafyi@users.noreply.github.com>
2020-10-05 22:51:27 +0700

    ui: add custom tailwind styles example

--- templates/mdnext-tailwind/pages/index.js
+++ templates/mdnext-tailwind/pages/index.js
@@ -1,7 +1,9 @@
 export default function Index() {
   return (
     <div>
-      <h1>Welcome to MDNEXT base</h1>
+      <h1 className="tomato-box text-center p-4">
+        Welcome to MDNEXT + Tailwind
+      </h1>
     </div>
   );
 }
