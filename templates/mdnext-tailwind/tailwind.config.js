e3d9ad3b5c3ce984a66badde41721f3eb1eaae2a
Eka <6597211+ekafyi@users.noreply.github.com>
2020-10-05 23:31:19 +0700

    modify postCSS config and setup tailwind purge

--- templates/mdnext-tailwind/tailwind.config.js
+++ templates/mdnext-tailwind/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ['./**/*.{js,jsx,ts,tsx,mdx}'],
   theme: {
     extend: {},
   },
