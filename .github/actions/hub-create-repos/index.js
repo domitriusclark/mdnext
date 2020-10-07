22a97e4109c8e18cf7a70c8128156ee30d7b845d
Jordan Nielson <jnielson94@gmail.com>
2020-10-06 20:24:37 -0600

    Add promises to wait for repo creation to finish

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -5,10 +5,27 @@ async function run() {
   try {
     console.debug('Starting the hub-create-repos action');
     const dirs = fs.readdirSync(process.env.GITHUB_WORKSPACE + '/templates');
-    console.debug(`Found the following templates: ${JSON.stringify(dirs)}`)
+    console.debug(`Found the following templates: ${JSON.stringify(dirs)}`);
+    const promises = [];
     for (let dir of dirs) {
-      exec(`gh repo create ${dir} --public --enable-issues=false`);
+      promises.push(
+        new Promise((resolve, reject) => {
+          exec(
+            `gh repo create ${dir} --public --enable-issues=false`,
+            (err, stdout, stderr) => {
+              if (err) {
+                console.error('Failed to create repo');
+                console.error(err);
+                console.error(stderr);
+                resolve('failed');
+              }
+              resolve('success');
+            },
+          );
+        }),
+      );
     }
+    await Promise.all(promises);
     console.debug('Finished creating repos');
   } catch (error) {
     console.error('Failed in hub-create-repos action');
