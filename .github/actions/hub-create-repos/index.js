f6e6a756f9652eb40fd73d5385d2dab6ba8c0fbd
Jordan Nielson <jnielson94@gmail.com>
2020-10-06 20:40:54 -0600

    Edit some logs, try to figure out why it hung

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -3,22 +3,24 @@ const fs = require('fs');
 
 async function run() {
   try {
-    console.debug('Starting the hub-create-repos action');
+    console.log('Starting the hub-create-repos action');
     const dirs = fs.readdirSync(process.env.GITHUB_WORKSPACE + '/templates');
-    console.debug(`Found the following templates: ${JSON.stringify(dirs)}`);
+    console.log(`Found the following templates: ${JSON.stringify(dirs)}`);
     const promises = [];
     for (let dir of dirs) {
       promises.push(
         new Promise((resolve, reject) => {
+          console.log('Kicking off creating repo for ', dir);
           exec(
             `gh repo create ${dir} --public --enable-issues=false`,
             (err, stdout, stderr) => {
               if (err) {
-                console.error('Failed to create repo');
+                console.error('Failed to create repo for ', dir);
                 console.error(err);
                 console.error(stderr);
                 resolve('failed');
               }
+              console.log('Finished creating repo for ', dir);
               resolve('success');
             },
           );
@@ -26,11 +28,11 @@ async function run() {
       );
     }
     const results = await Promise.all(promises);
-    if (results.some((result, index) => result === 'failed')) {
-      throw new Error(`Failed to create repo for ${dirs[index]}`);
+    if (results.some((result) => result === 'failed')) {
+      throw new Error(`Failed to create a repo`);
     }
 
-    console.debug('Finished creating repos');
+    console.log('Finished creating repos');
   } catch (error) {
     console.error('Failed in hub-create-repos action');
     console.error(error.message);
