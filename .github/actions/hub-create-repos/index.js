9a03512211145cca9b7ea8e3bf680836f8d27f16
Jordan Nielson <jnielson94@gmail.com>
2020-10-06 20:18:28 -0600

    Add logs

--- .github/actions/hub-create-repos/index.js
+++ .github/actions/hub-create-repos/index.js
@@ -3,13 +3,16 @@ const fs = require('fs');
 
 async function run() {
   try {
+    console.debug('Starting the hub-create-repos action');
     const dirs = fs.readdirSync(process.env.GITHUB_WORKSPACE + '/templates');
-
+    console.debug(`Found the following templates: ${JSON.stringify(dirs)}`)
     for (let dir of dirs) {
       exec(`gh repo create ${dir} --public --enable-issues=false`);
     }
+    console.debug('Finished creating repos');
   } catch (error) {
-    console.log(error.message);
+    console.error('Failed in hub-create-repos action');
+    console.error(error.message);
   }
 }
 
