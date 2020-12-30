89aca3c12ed81f259bdb6a0414373c77381872e1
Benjamin Lannon <benjamin@lannonbr.com>
2020-12-30 12:29:32 -0500

    Upgrade mdnext-tailwind to use Tailwind 2.0

--- templates/mdnext-tailwind/tailwind.config.js
+++ templates/mdnext-tailwind/tailwind.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  future: {
-    // Enable these to remove CLI warnings.
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
   // Purge unused Tailwind styles in production to keep build CSS file small.
   purge: ['./**/*.{js,jsx,ts,tsx,mdx}'],
   theme: {
