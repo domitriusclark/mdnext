b8be783855e703a4b1781fca9729764f6e4f3aff
DomitriusAnthony <domitriusaclark@gmail.com>
2021-01-16 05:34:12 -0500

    Upgraded react chakra and react-dom in mdnext-components and upgraded the package

--- packages/mdnext-components/src/components/Image.js
+++ packages/mdnext-components/src/components/Image.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image as ChakraImage } from '@chakra-ui/core';
+import { Image as ChakraImage } from '@chakra-ui/react';
 import { useImage } from 'use-cloudinary';
 
 export default function Image({
@@ -65,7 +65,7 @@ export default function Image({
               <ChakraImage
                 src={generateImageUrl({
                   delivery: { publicId },
-                  transformation: { ...transforms }
+                  transformation: { ...transforms },
                 })}
                 loading="lazy"
                 width="100%"
