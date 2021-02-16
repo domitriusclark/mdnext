acff486d74ce8420a2d031869d3baf2827189dbc
Alex Low <me@alexlow.dev>
2021-02-15 22:27:50 -0500

    changed: global share image gen

--- templates/mdnext-blog/src/components/Layout.js
+++ templates/mdnext-blog/src/components/Layout.js
@@ -1,15 +1,26 @@
+import { getShareImage } from '@jlengstorf/get-share-image';
 import { Container } from '@chakra-ui/react';
 import Navbar from '@components/Navbar';
 import SEO from '@components/SEO';
 
 export function Layout({ title, description, twitter, openGraph, children }) {
+  const socialImage = getShareImage({
+    title: 'MDNext Blog Starter',
+    tagline: 'A test of sorts.',
+    cloudName: process.env.CLOUDINARY_USER_ID,
+    imagePublicID: process.env.CLOUDINARY_IMAGE_ID,
+    titleFont: 'Montserrat',
+    taglineFont: 'Montserrat',
+    textColor: '000000',
+  });
+
   return (
     <>
       <SEO
         title={title}
         description={description}
         twitter={twitter}
-        openGraph={openGraph}
+        openGraph={openGraph ? openGraph : socialImage}
       />
       <Navbar />
       <Container maxW="lg">{children}</Container>
