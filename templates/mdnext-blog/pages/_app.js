61c7e99adcdccdbc1fa2bb39c55d0f9c1b4c0684
Alex Low <me@alexlow.dev>
2021-02-15 22:45:25 -0500

    changed: default share card

--- templates/mdnext-blog/pages/_app.js
+++ templates/mdnext-blog/pages/_app.js
@@ -1,6 +1,16 @@
 import { Chakra } from '@components/Chakra';
 import { DefaultSeo } from 'next-seo';
+import getShareImage from '@jlengstorf/get-share-image';
 
+const socialImage = getShareImage({
+  title,
+  tagline: 'A test of sorts.',
+  cloudName: process.env.CLOUDINARY_USER_ID,
+  imagePublicID: process.env.CLOUDINARY_IMAGE_ID,
+  titleFont: 'Montserrat',
+  taglineFont: 'Montserrat',
+  textColor: '000000',
+});
 
 const App = ({ Component, pageProps, cookies }) => {
   return (
@@ -9,16 +19,23 @@ const App = ({ Component, pageProps, cookies }) => {
         title="Nextjs + MDX Starter pack"
         description="This is an opinionated way to handle MDX from multiple sources in a Next project with some help styling from ChakraUI"
         url="www.whatever.com"
-        ogImage={{
-          url: 'www.whatever.com',
-          title: 'OG Image title',
-          description: 'Describe the OG image',
-          image: ``,
-          siteName: 'Your site name',
+        openGraph={{
+          title: 'Nextjs + MDX Starter pack',
+          description: 'MDNext Title Card',
+          images: [
+            {
+              url: socialImage,
+              width: 800,
+              height: 418,
+              alt: 'Nextjs + MDX Starter pack',
+            },
+          ],
         }}
         twitter={{
           handle: '@domitriusclark',
           site: 'https://twitter.com/domitriusclark',
+          title: 'Nextjs + MDX Starter pack',
+          cardType: 'summary_large_image',
         }}
       />
       <Component {...pageProps} />
--- templates/mdnext-blog/src/components/Layout.js
+++ templates/mdnext-blog/src/components/Layout.js
@@ -1,18 +1,9 @@
-import getShareImage from '@jlengstorf/get-share-image';
 import { Container } from '@chakra-ui/react';
 import Navbar from '@components/Navbar';
 import SEO from '@components/SEO';
 
 export function Layout({ title, description, twitter, openGraph, children }) {
-  const socialImage = getShareImage({
-    title: 'MDNext Blog Starter',
-    tagline: 'A test of sorts.',
-    cloudName: process.env.CLOUDINARY_USER_ID,
-    imagePublicID: process.env.CLOUDINARY_IMAGE_ID,
-    titleFont: 'Montserrat',
-    taglineFont: 'Montserrat',
-    textColor: '000000',
-  });
+
 
   return (
     <>
@@ -20,7 +11,7 @@ export function Layout({ title, description, twitter, openGraph, children }) {
         title={title}
         description={description}
         twitter={twitter}
-        openGraph={openGraph ? openGraph : socialImage}
+        openGraph={openGraph}
       />
       <Navbar />
       <Container maxW="lg">{children}</Container>
