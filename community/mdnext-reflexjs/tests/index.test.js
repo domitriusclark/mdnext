c518323ab5c1eb37e8a25666456521510c445047
DomitriusAnthony <domitriusaclark@gmail.com>
2021-02-27 02:57:40 -0500

    Removing community templates from the repo

--- community/mdnext-reflexjs/tests/index.test.js
+++
@@ -1,7 +0,0 @@
-import { formatDate } from '../src/utils/format-date';
-
-describe('format date', () => {
-  test('converts string date into human-friendly date', () => {
-    expect(formatDate('2020-11-20')).toEqual('November 20, 2020');
-  });
-});
